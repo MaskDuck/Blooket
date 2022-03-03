d00627e74bf3404f4eca1274b474e50addd43cfc
Minh Dương <i-am@maskduck.ninja>
2022-03-03 13:29:31 +0700

    pep8 things

--- py/example.py
+++ py/example.py
@@ -1,4 +1,6 @@
-import blooket # may need to change based on local path
-name = "Goosey";
-blooket.login("youremail", "yourpassword"); #  !! this part is required  !!
-print(blooket.formatBlookString(name));
+import blooket  # may need to change based on local path
+
+name = "Goosey"
+blooket.login("youremail", "yourpassword")
+#  !! this part is required  !!
+print(blooket.formatBlookString(name))
